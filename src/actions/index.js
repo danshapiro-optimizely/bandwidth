375c4d503ec338ff7c25cacd77b271737ea6b047
dshaps10 <dan.shapiro1210@gmail.com>
2017-06-09 08:20:59 -0700

    Add fetchData() action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
+
+// List of all opps
+export const FETCH_OPPS = 'fetch_opps';
+
+const ROOT_URL = 'localhost:8080/api';
+
 export function fetchData() {
-  const data = 'This is example data';
+  const request = axios.get(`${ROOT_URL}`);
 
   return {
-    type: 'example_data',
-    payload: data
+    type: FETCH_OPPS,
+    payload: request
   }
 }
